5a97ca0c8e6ead44ad815a805fcdba554d002786
abkalen778-eng <abkalen778@gmail.com>
2026-08-23 20:23:42 -0400

    Refresh Black Hole app cache for Farms tab

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='black-hole-v2';
-const ASSETS=['./','./index.html','./manifest.json'];
+const CACHE='black-hole-v4';
+const ASSETS=['./','./index.html','./manifest.json','./bots.html','./data.html','./scanners.html'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',event=>{
